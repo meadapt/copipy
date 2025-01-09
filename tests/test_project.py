6541b05cce02e40aafe90867ce4926498ca414b7
Gabriel Dornas <gabrielbdornas@gmail.com>
2025-01-09 18:31:52 -0300

    Improve project first/initial tests with Pathlib
    Fix #2

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pytest
 
 @pytest.fixture(scope='session')
@@ -7,10 +8,14 @@ def copier_project_defaults():
             "module_name": "test_module",
         }
 
-def test_project_folder(copie, copier_project_defaults):
+def test_project_folder_creation(copie, copier_project_defaults):
     project_defaults = copier_project_defaults
     project = copie.copy(extra_answers=project_defaults)
+    project_folders = [folder for folder in project.project_dir.glob('*')]
+    project_path = project_folders[0]
 
     assert project.exit_code == 0
     assert project.exception is None
-    assert project.project_dir.is_dir()
+    assert len(project_folders) == 1
+    assert project_path.is_dir()
+    assert project_path.stem == project.answers['project_name']
