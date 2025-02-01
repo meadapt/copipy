b5246025b40c3755ad833511a3f3fda736208b52
Gabriel Dornas <gabrielbdornas@gmail.com>
2025-01-31 22:40:00 -0300

    Improve tests for project script's folder
    The project_scripts_dir_name must not start with numbers and this test is not passing yet.
    The project_dir_name can start with number but the project_scripts_dir_name not.
    It could cause SyntaxError as shown below
    
    >>> from 1test_py import main
      File "<stdin>", line 1
        from 1test_py import main
             ^
    SyntaxError: invalid decimal literal
    
    See #5
    See #9
    See #13

---
+++ tests/test_project_structure.py
@@ -0,0 +1,27 @@
+from tests.conftest import project_dir
+
+import pytest
+
+@pytest.mark.parametrize('project_name, project_scripts_dir_name', [
+    ('New Python project', 'new_python_project'),
+    ('1Meu Lindo Projeto', 'meu_lindo_projeto'),
+    ('MEU LINDO PROJETO', 'meu_lindo_projeto'),
+    ('meu-lindo-projeto', 'meu_lindo_projeto'),
+    ('meu_lindo_projeto', 'meu_lindo_projeto'),
+    ('Python Copier COM Café', 'python_copier_com_cafe'),
+    ('PYTHON COPIER COM VOCÊ', 'python_copier_com_voce'),
+    ('PYTHON copier com fumaça', 'python_copier_com_fumaca'),
+    ('copier à Grega', 'copier_a_grega'),
+    ('copier_à_Grega', 'copier_a_grega'),
+    ('copier___à_ Grega', 'copier_a_grega'),
+    ('copipy', 'copipy'),
+    ('COPIPY', 'copipy'),
+    ('Copipy', 'copipy'),
+])
+def test_project_structure_createion(create_project, project_name, project_scripts_dir_name):
+
+    project = create_project(overrides={'project_name': project_name})
+    _project_dir = project_dir(project)
+    project_path_structure = _project_dir[3]
+
+    assert project_scripts_dir_name in project_path_structure
